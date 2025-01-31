f8f7eac370bb4323133b99aa6efd40239d6c2b5c
rkddusen <k99endus@naver.com>
2025-01-31 21:39:21 +0900

    solve

--- 골드2/컵라면/index.js
+++ 골드2/컵라면/index.js
@@ -1,20 +1,13 @@
-class MinHeap {
+class MaxHeap {
   constructor() {
     this.heap = [];
-  }
-  compare(a, b) {
-    if (a[1] !== b[1]) return b[1] - a[1];
-    return b[0] - a[0];
   }
   add(val) {
     this.heap.push(val);
 
     let curIdx = this.heap.length - 1;
     let parIdx = Math.floor((curIdx - 1) / 2);
-    while (
-      this.heap[parIdx] &&
-      this.compare(this.heap[curIdx], this.heap[parIdx]) < 0
-    ) {
+    while (this.heap[parIdx] && this.heap[curIdx] > this.heap[parIdx]) {
       [this.heap[parIdx], this.heap[curIdx]] = [
         this.heap[curIdx],
         this.heap[parIdx],
@@ -33,16 +26,11 @@ class MinHeap {
       let curIdx = 0;
       let [leftIdx, rightIdx] = [curIdx * 2 + 1, curIdx * 2 + 2];
       while (
-        (this.heap[leftIdx] &&
-          this.compare(this.heap[leftIdx], this.heap[curIdx]) < 0) ||
-        (this.heap[rightIdx] &&
-          this.compare(this.heap[rightIdx], this.heap[curIdx]) < 0)
+        (this.heap[leftIdx] && this.heap[leftIdx] > this.heap[curIdx]) ||
+        (this.heap[rightIdx] && this.heap[rightIdx] > this.heap[curIdx])
       ) {
         let smallIdx = leftIdx;
-        if (
-          this.heap[rightIdx] &&
-          this.compare(this.heap[rightIdx], this.heap[leftIdx]) < 0
-        ) {
+        if (this.heap[rightIdx] && this.heap[rightIdx] > this.heap[leftIdx]) {
           smallIdx = rightIdx;
         }
         [this.heap[smallIdx], this.heap[curIdx]] = [
@@ -65,23 +53,28 @@ let [[N], ...problem] = require("fs")
   .split("\n")
   .map((v) => v.split(" ").map(Number));
 
-const heap = new MinHeap();
-for (let i = 0; i < N; i++) {
-  heap.add(problem[i]);
-}
+problem.sort((a, b) => b[0] - a[0]);
 
-let ramen = new Array(N + 1).fill(0);
+const heap = new MaxHeap();
+let line = problem[0][0];
 let answer = 0;
 for (let i = 0; i < N; i++) {
-  let [index, value] = heap.shift();
-
-  while (index > 0) {
-    if (!ramen[index]) {
-      ramen[index] = value;
-      answer += value;
-      break;
-    } else index--;
+  if (problem[i][0] === line) {
+    heap.add(problem[i][1]);
+  } else {
+    while (problem[i][0] < line) {
+      let value = heap.shift();
+      if (value !== null) answer += value;
+      line--;
+    }
+    heap.add(problem[i][1]);
   }
+}
+
+while (line > 0) {
+  let value = heap.shift();
+  if (value !== null) answer += value;
+  line--;
 }
 
 console.log(answer);
