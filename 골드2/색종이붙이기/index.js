29aafb255c02e51f87f133b77f487a1c56330aba
rkddusen <k99endus@naver.com>
2025-03-30 23:54:45 +0900

    solve

--- 골드2/색종이붙이기/index.js
+++ 골드2/색종이붙이기/index.js
@@ -7,15 +7,13 @@ const [...input] = require("fs")
   .map((v) => v.split(" ").map(Number));
 
 const possible = [5, 5, 5, 5, 5]; // 순서대로 1*1, 2*2, 3*3, 4*4, 5*5 남은 수
-const rect = [];
-// 이미 색종이 붙인 곳인지 체크
-const isInRect = (x, y) => {
-  for (let i = 0; i < rect.length; i++) {
-    const [x1, y1, x2, y2] = rect[i];
-    if (x1 <= x && x <= x2 && y1 <= y && y <= y2) return true;
+// 색종이 붙였다 떼기
+const action = (x, y, cnt, temp) => {
+  for (let a = 0; a < cnt; a++) {
+    for (let b = 0; b < cnt; b++) {
+      input[x + a][y + b] = temp ? cnt : 1;
+    }
   }
-
-  return false;
 };
 let left = 0;
 for (let i = 0; i < 10; i++) {
@@ -24,22 +22,21 @@ for (let i = 0; i < 10; i++) {
   }
 }
 let answer = Infinity;
-const dfs = (x, y) => {
+const dfs = (x, y, count) => {
   if (left === 0) {
-    console.log(rect);
-    answer = Math.min(rect.length, answer);
+    answer = Math.min(count, answer);
     return;
   }
-  if (rect.length >= answer) return;
+  if (count >= answer) return;
   for (let idx = 10 * x + y; idx < 100; idx++) {
     const [i, j] = [Math.floor(idx / 10), idx % 10];
-    if (input[i][j] === 1 && !isInRect(i, j)) {
+    if (input[i][j] === 1) {
       // 5*5
       if (i <= 5 && j <= 5 && possible[4] > 0) {
         let fail = false;
         for (let a = 0; a < 5; a++) {
           for (let b = 0; b < 5; b++) {
-            if (input[i + a][j + b] === 0) {
+            if (input[i + a][j + b] !== 1) {
               fail = true;
               break;
             }
@@ -47,12 +44,12 @@ const dfs = (x, y) => {
           if (fail) break;
         }
         if (!fail) {
-          rect.push([i, j, i + 4, j + 4]);
+          action(i, j, 5, 1);
           possible[4]--;
           left -= 25;
-          if (j === 5) dfs(i + 1, 0);
-          else dfs(i, j + 5);
-          rect.pop();
+          if (j === 5) dfs(i + 1, 0, count + 1);
+          else dfs(i, j + 5, count + 1);
+          action(i, j, 5, 0);
           possible[4]++;
           left += 25;
         }
@@ -62,7 +59,7 @@ const dfs = (x, y) => {
         let fail = false;
         for (let a = 0; a < 4; a++) {
           for (let b = 0; b < 4; b++) {
-            if (input[i + a][j + b] === 0) {
+            if (input[i + a][j + b] !== 1) {
               fail = true;
               break;
             }
@@ -70,12 +67,12 @@ const dfs = (x, y) => {
           if (fail) break;
         }
         if (!fail) {
-          rect.push([i, j, i + 3, j + 3]);
+          action(i, j, 4, 1);
           possible[3]--;
           left -= 16;
-          if (j === 6) dfs(i + 1, 0);
-          else dfs(i, j + 4);
-          rect.pop();
+          if (j === 6) dfs(i + 1, 0, count + 1);
+          else dfs(i, j + 4, count + 1);
+          action(i, j, 4, 0);
           possible[3]++;
           left += 16;
         }
@@ -85,7 +82,7 @@ const dfs = (x, y) => {
         let fail = false;
         for (let a = 0; a < 3; a++) {
           for (let b = 0; b < 3; b++) {
-            if (input[i + a][j + b] === 0) {
+            if (input[i + a][j + b] !== 1) {
               fail = true;
               break;
             }
@@ -93,12 +90,12 @@ const dfs = (x, y) => {
           if (fail) break;
         }
         if (!fail) {
-          rect.push([i, j, i + 2, j + 2]);
+          action(i, j, 3, 1);
           possible[2]--;
           left -= 9;
-          if (j === 7) dfs(i + 1, 0);
-          else dfs(i, j + 3);
-          rect.pop();
+          if (j === 7) dfs(i + 1, 0, count + 1);
+          else dfs(i, j + 3, count + 1);
+          action(i, j, 3, 0);
           possible[2]++;
           left += 9;
         }
@@ -108,7 +105,7 @@ const dfs = (x, y) => {
         let fail = false;
         for (let a = 0; a < 2; a++) {
           for (let b = 0; b < 2; b++) {
-            if (input[i + a][j + b] === 0) {
+            if (input[i + a][j + b] !== 1) {
               fail = true;
               break;
             }
@@ -116,12 +113,12 @@ const dfs = (x, y) => {
           if (fail) break;
         }
         if (!fail) {
-          rect.push([i, j, i + 1, j + 1]);
+          action(i, j, 2, 1);
           possible[1]--;
           left -= 4;
-          if (j === 8) dfs(i + 1, 0);
-          else dfs(i, j + 2);
-          rect.pop();
+          if (j === 8) dfs(i + 1, 0, count + 1);
+          else dfs(i, j + 2, count + 1);
+          action(i, j, 2, 0);
           possible[1]++;
           left += 4;
         }
@@ -131,7 +128,7 @@ const dfs = (x, y) => {
         let fail = false;
         for (let a = 0; a < 1; a++) {
           for (let b = 0; b < 1; b++) {
-            if (input[i + a][j + b] === 0) {
+            if (input[i + a][j + b] !== 1) {
               fail = true;
               break;
             }
@@ -139,12 +136,12 @@ const dfs = (x, y) => {
           if (fail) break;
         }
         if (!fail) {
-          rect.push([i, j, i, j]);
+          action(i, j, 1, 1);
           possible[0]--;
           left -= 1;
-          if (j === 9) dfs(i + 1, 0);
-          else dfs(i, j + 1);
-          rect.pop();
+          if (j === 9) dfs(i + 1, 0, count + 1);
+          else dfs(i, j + 1, count + 1);
+          action(i, j, 1, 0);
           possible[0]++;
           left += 1;
         }
@@ -153,5 +150,5 @@ const dfs = (x, y) => {
     }
   }
 };
-dfs(0, 0);
+dfs(0, 0, 0);
 console.log(answer === Infinity ? -1 : answer);
